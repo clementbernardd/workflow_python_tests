342f350b8cc13e86e4c4c5108bb2e3912e2cd7ba
clementbernardd <clementbernardd@gmail.com>
2022-11-30 11:59:03 +0100

    update(entrypoint): fix bad requirements and merge of local files to repo files

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ COPY requirements.txt requirements.txt
 RUN apt-get update && apt-get install -y bc make
 
 RUN pip install -r requirements.txt
-RUN set -euxo # Enable to stop the code when there is an error
 
 COPY script/lintage /app/script/lintage
 COPY entrypoint.sh /app
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,27 @@
 #!/bin/sh -l
+# Relative path in the container
+CODE_PATH=/app/code
+
 
 # Export variables from --env docker arguments
-export PATH_TO_CODE=$INPUT_WORKDIR
-export PATH_TO_UNIT_TESTS=$INPUT_TEST_DIR
-export MAX_LINE_LENGTH=$INPUT_MAX_LINE_LENGTH
-export COMPLEXITY_SCORE=$INPUT_COMPLEXITY_SCORE
-export COVERAGE_SCORE=$INPUT_COVERAGE_SCORE
-export DOCUMENTATION_SCORE=$INPUT_DOCUMENTATION_SCORE
+export PATH_TO_CODE=$CODE_PATH/$INPUT_WORKDIR
+export PATH_TO_UNIT_TESTS=$CODE_PATH/$INPUT_TEST_DIR
+export MAX_LINE_LENGTH=$CODE_PATH/$INPUT_MAX_LINE_LENGTH
+export COMPLEXITY_SCORE=$CODE_PATH/$INPUT_COMPLEXITY_SCORE
+export COVERAGE_SCORE=$CODE_PATH/$INPUT_COVERAGE_SCORE
+export DOCUMENTATION_SCORE=$CODE_PATH/$INPUT_DOCUMENTATION_SCORE
 
-## As the workdir for github actions isn't the default one, copy the testing commands here.
-cp -r /app/* .
-# Install missing libraries if neccessary
+set -euxo # Enable to stop the code when there is an error
+
+# Install missing libraries if necessary
 if [ -f "requirements.txt" ]; then
   pip install -r requirements.txt
 fi
+
+## As the workdir for github actions isn't the default one, copy the testing commands in /app/code
+mkdir /app/$CODE_PATH
+cp -r ./ /app/$CODE_PATH/
+cd /app
+
 # Command to launch all the tests at once
 make all_tests
